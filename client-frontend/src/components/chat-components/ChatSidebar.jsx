dd84ae8d4572a6b71178741e203db08b875b0829
sankalp-wono <sankalp.wono@gmail.com>
2024-11-11 18:31:29 +0530

    improved the chat UI

--- client-frontend/src/components/chat-components/ChatSidebar.jsx
+++ client-frontend/src/components/chat-components/ChatSidebar.jsx
@@ -8,16 +8,15 @@ export default function ChatSidebar() {
   const [data, setData] = useState(flattenData);
 
   return (
-    <aside className="min-h-screen w-64 overflow-y-auto">
-      <h2 className="text-2xl font-bold p-4 sticky">Chats</h2>
-      <div className="w-full h-full p-4 border-l border-t border-gray-300 shadow-lg bg-white">
-        {data.map((item, index) =>
-          item.email !== storedUser.email ? (
-            <Chat key={index} item={item} />
-          ) : null
-        )}
-      </div>
-    </aside>
+      <aside className="min-h-screen w-64 overflow-y-auto">
+        <h2 className="text-2xl font-bold p-4 sticky">Chats</h2>
+        <div className="w-full h-full p-4 border-l border-t border-gray-300 shadow-lg bg-white">
+          {data.map((item, index) =>
+            item.email !== storedUser.email ? (
+              <Chat key={index} item={item} />
+            ) : null
+          )}
+        </div>
+      </aside>
   );
-  
 }
