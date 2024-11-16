91a542edb29b03dc476b227f58d0bac02a461dd7
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2024-11-16 16:18:07 +0530

    Added footer

--- client-frontend/src/components/ClientFooter.jsx
+++ client-frontend/src/components/ClientFooter.jsx
@@ -3,12 +3,16 @@ import WonoLogo from '../assets/BIZNest/biznest_logo.jpg'
 
 const ClientFooter = () => {
   return (
-    <footer className="bg-gray-900 text-white py-6 ">
+    <footer className="bg-black text-white py-6 ">
       <div className="container mx-auto flex flex-col items-center space-y-4">
         {/* Logo */}
-        <div className="text-2xl font-bold flex">
-          <span>Powered by WoNo</span> 
-        </div>      
+        {/* <div className="text-3xl font-bold flex">
+          <span>Powered by W<span className="wono-blue-text">o</span>N<span className="wono-blue-text">o</span></span> 
+        </div>   */}
+
+         <p className="text-sm text-gray-400 text-center">
+          © Copyright 2024-25 by WONOCO PRIVATE-LIMITED - SINGAPORE . All rights reserved.
+        </p>    
       </div>
     </footer>
   );
