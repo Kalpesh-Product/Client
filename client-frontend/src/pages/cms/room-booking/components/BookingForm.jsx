c0e8160b20892653bd249fb8234f6410f60bb97c
sankalp-wono <sankalp.wono@gmail.com>
2024-12-05 10:25:05 +0530

    some changes

--- client-frontend/src/pages/cms/room-booking/components/BookingForm.jsx
+++ client-frontend/src/pages/cms/room-booking/components/BookingForm.jsx
@@ -79,7 +79,7 @@ export default function BookingForm({
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"en-gb"}>
       <div className="mx-auto p-6 bg-white w-[50vw]">
         <FormStepper steps={steps} handleClose={handleClose}>
           {(activeStep, handleNext) => {
