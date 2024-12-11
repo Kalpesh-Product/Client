b8f3afb5cb4b1325b7d1b191a92d3e6661126c37
sankalp-wono <sankalp.wono@gmail.com>
2024-12-11 11:18:08 +0530

    refactored routes and controllers in backend

--- client-backend/routes/assetsRoutes.js
+++ client-backend/routes/assetsRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {
   addAsset,
-} = require("../../controllers/assetsControllers/assetsControllers");
+} = require("../controllers/assetsControllers/assetsControllers");
 
 router.post("/create-asset", addAsset);
 
